d9cbe010707953c7711b859a1bcdc97a8a9faec9
Karina Desyatnik <karinadesyatnik27@gmail.com>
2019-03-12 09:13:18 +0200

    homework13

--- homework.js
+++ homework.js
@@ -1,2 +1,15 @@
 // -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Десятник Карина --------------------------
+;(function ($) {   
+    $('a').on('click', function(e){
+      e.preventDefault();
+    });    
+    $('.dropdown-menu li').hover(function () {
+      $('ul',this).slideDown();
+      $('li', this).css('background', '#9a59b5');
+      $(this).css('background', '#9a59b5');
+    }, function () { 
+      $('ul',this).slideUp(); 
+      $(this).css('background', 'transparent');  
+    });
+  }(jQuery));
